d5d29d04f329af417eb360e73cee5fb686d088f3
Vishal Menon <vishalmenon.92@gmail.com>
2021-10-10 22:22:53 +0530

    Updated schnorr.rs struct naming

--- packages/bitcoin/rust/src/key/schnorr.rs
+++ packages/bitcoin/rust/src/key/schnorr.rs
@@ -1,4 +1,4 @@
-use serde::{Serialize,Deserialize};
+// use serde::{Serialize,Deserialize};
 use crate::e::{ErrorKind,S5Error};
 
 use secp256k1::rand::rngs::OsRng;
@@ -6,10 +6,9 @@ use secp256k1::Secp256k1;
 
 use bitcoin::util::schnorr::{KeyPair,PublicKey};
 
-
 #[derive(Debug,Clone)]
 pub struct SchnorrPair{
-  private_key: KeyPair,
+  key_pair: KeyPair,
   public_key: PublicKey
 }
 
@@ -20,11 +19,12 @@ pub fn generate()->Result<SchnorrPair,S5Error>{
     Err(_) => return Err(S5Error::new(ErrorKind::OpError,"OS-RNG")),
   };
   let secp = Secp256k1::new();
-  let private_key = KeyPair::new(&secp,&mut rng);
-  let public_key = PublicKey::from_keypair(&secp,&private_key);
+
+  let key_pair = KeyPair::new(&secp,&mut rng);
+  let public_key = PublicKey::from_keypair(&secp,&key_pair);
 
   Ok(SchnorrPair{
-    private_key,
+    key_pair,
     public_key
   })
 
@@ -35,7 +35,7 @@ mod tests {
   use super::*;
   #[test]
   fn test_schnorr() {
-    let keypair = generate();
-    println!("{:#?}",keypair);
+    let schnorr_keys = generate();
+    println!("{:#?}",schnorr_keys);
   }
 }
