19d55d15b5ccb05ae842ba1482e417fc9c8d1206
mj <jmorteza@hotmail.com>
2021-10-29 12:54:46 +0530

    ios working

---
+++ ios/Runner/Bitcoin-Bridging-Header.h
@@ -0,0 +1,12 @@
+//
+//  Bitcoin-Bridging-Header.h
+//  Runner
+//
+//  Created by MJ on 29/10/21.
+//
+
+#ifndef Bitcoin_Bridging_Header_h
+#define Bitcoin_Bridging_Header_h
+
+
+#endif /* Bitcoin_Bridging_Header_h */
